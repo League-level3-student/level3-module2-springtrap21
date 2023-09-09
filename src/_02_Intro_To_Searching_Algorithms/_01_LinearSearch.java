3d7e75e2ea56618076b7e993e895477db251fcd8
drake <drake@GlizzyGobbler>
2023-09-09 11:28:27 -0700

    Lsearch

--- src/_02_Intro_To_Searching_Algorithms/_01_LinearSearch.java
+++ src/_02_Intro_To_Searching_Algorithms/_01_LinearSearch.java
@@ -1,17 +1,30 @@
 package _02_Intro_To_Searching_Algorithms;
 
-public class _01_LinearSearch {
-    /*
-     * When finding the location of a certain element in an array,
-     * there are many algorithms that can do so. The simplest is the 
-     * linear search. This is best for short unsorted lists of data.
-     */
-    public static int linearSearch(String[] words, String value) {
-        // 1. Complete the method using a for loop to find the value
-        //    in the array. Return the location in the array
-        //    where the value was found.
-        //    If the value is not found in the array, return -1.
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-        return 0;
-    }
+import java.util.Arrays;
+
+public class _01_LinearSearch {
+	/*
+	 * When finding the location of a certain element in an array, there are many
+	 * algorithms that can do so. The simplest is the linear search. This is best
+	 * for short unsorted lists of data. // 1. Complete the method using a for loop
+	 * to find the value // in the array. Return the location in the array // where
+	 * the value was found. // If the value is not found in the array, return -1.
+	 */
+	String[] words = { "hi", "bye", "hello", "yes", "no" };
+	int index = linearSearch(words, "yes");
+
+	public static int linearSearch(String[] words, String value) {
+		System.out.println("Using Linear Search on array to find: " + value + " in " + Arrays.toString(words));
+		for (int i = 0; i < words.length; i++) {
+			if (words[i] == value) {
+				System.out.println(value + " found at index: " + i);
+				return i;
+			}
+		}
+		return -1;
+
+	}
 }

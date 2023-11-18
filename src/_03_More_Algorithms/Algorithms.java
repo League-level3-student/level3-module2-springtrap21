cdb29158efaf2c29132d025ef7862f3a6e66342f
drake <drake@GlizzyGobbler>
2023-11-18 11:18:31 -0800

    oystyers

--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -6,17 +6,32 @@ import java.util.List;
  * Write your methods in this class
  */
 public class Algorithms {
-    
-    public static String multiply(int num1, int num2) {
-        return null;
-    }
-    
-    public static int findBrokenEgg(List<String> eggs) {
-    	for (int i = 0; i < eggs.size(); i++) {
-			if (eggs.get(i) == "cracked") {
+
+	public static String multiply(int num1, int num2) {
+		return null;
+	}
+
+	public static int findBrokenEgg(List<String> eggs) {
+//	This is how a for each loop works
+//    	for (String egg : eggs) {
+//			if (egg.equals("cracked")) {
+//				return i;
+//			}
+//		}
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i).equals("cracked")) {
 				return i;
 			}
 		}
-        return -1;
-    }
+		return -1;
+	}
+
+	public static int countPearls(List<Boolean> oysters) {
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
